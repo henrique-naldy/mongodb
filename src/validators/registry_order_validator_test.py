3fa10fc3aa400f4d533283bbef25c336cb7f6418
Henrique Naldy <Henrique.nordy@gmail.com>
2025-03-23 15:01:27 -0300

    feat Implementing registry_order_validator

---
+++ src/validators/registry_order_validator_test.py
@@ -0,0 +1,31 @@
+import pytest
+from .registry_order_validator import registry_order_validator
+
+def test_registry_order_validator():
+    body = {
+        "data": {
+            "name": "joaozinho",
+            "address": "rua do limao",
+            "cupom": False,
+            "items": [
+                { "item": "Regrigerante", "quantidade": 2 },
+                { "item": "pizza", "quantidade": 3 }
+            ] 
+        }
+    }
+    registry_order_validator(body)
+
+def test_registry_order_validator_with_errors():
+    body_with_error = {
+        "data": {
+            "name": "joaozinho",
+            "address": "rua do limao",
+            "cupom": "error",
+            "items": [
+                { "item": "Regrigerante", "quantidade": 2 },
+                { "item": "pizza", "quantidade": 3 }
+            ] 
+        }
+    }  
+    with pytest.raises(Exception):
+        registry_order_validator(body_with_error)  
